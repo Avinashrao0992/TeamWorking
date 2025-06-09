2bcac1279b8a8aa7da3e6bb1dbdef920a3640cbd
Avinashrao0992 <avinashraonanoore@gmail.com>
2025-06-09 19:48:06 +0530

    TL 2 Commit

--- Application/src/test/java/com/nt/Main/AppTest.java
+++ Application/src/test/java/com/nt/Main/AppTest.java
@@ -1,5 +1,6 @@
 package com.nt.Main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -9,11 +10,8 @@ import org.junit.jupiter.api.Test;
  */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue(true);
+       assertEquals("Hi", "Hi");
     }
 }
